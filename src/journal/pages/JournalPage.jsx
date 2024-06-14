bf982c432c6f7e3ec1f5405d9076ce72dc1af001
Oliver31Batista <oliver31batista@gmail.com>
2024-06-14 16:25:29 -0400

    Installation and configuration of MUI with vite

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,10 @@
+import { Typography } from "@mui/material";
 
 export const JournalPage = () => {
+  //I can use components or variants, components for the element html and variants for the theme
   return (
-    <div>JournalPage</div>
-  )
-}
+    <>
+      <Typography variant="h1">JournalPage</Typography>
+    </>
+  );
+};
