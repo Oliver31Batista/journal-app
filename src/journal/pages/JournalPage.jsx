909fc46515cc52c811bc8f1a69fda796c916424f
Oliver31Batista <oliver31batista@gmail.com>
2024-06-16 04:47:44 -0400

    NoteView an Image gallery created

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,12 @@
 import { JournalLayout } from "../layout/JournalLayout";
-import { NothingSelectedView } from "../views";
+import { NothingSelectedView, NoteView } from "../views";
 
 export const JournalPage = () => {
   //I can use components or variants, components for the element html and variants for the theme
   return (
     <JournalLayout>
-      <NothingSelectedView />
-      {/* NoteView */}
+      {/* <NothingSelectedView /> */}
+      <NoteView />
     </JournalLayout>
   );
 };
