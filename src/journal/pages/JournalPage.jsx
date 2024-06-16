2d15e4918e579760a4a1e470524dfde97522eda1
Oliver31Batista <oliver31batista@gmail.com>
2024-06-16 05:19:55 -0400

    Floating button to add a new entry created

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,6 @@
+import { AddOutlined } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+
 import { JournalLayout } from "../layout/JournalLayout";
 import { NothingSelectedView, NoteView } from "../views";
 
@@ -5,8 +8,22 @@ export const JournalPage = () => {
   //I can use components or variants, components for the element html and variants for the theme
   return (
     <JournalLayout>
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
