f5b61706b085770e9c2617abefcf89b49a52d60f
Oliver31Batista <oliver31batista@gmail.com>
2024-07-23 15:40:00 -0400

    delete a note

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,11 +1,19 @@
-import { SaveOutlined, UploadOutlined } from "@mui/icons-material";
+import {
+  DeleteOutline,
+  SaveOutlined,
+  UploadOutlined,
+} from "@mui/icons-material";
 import { Button, Grid, IconButton, TextField, Typography } from "@mui/material";
 import { ImageGallery } from "../components";
 import { useForm } from "../../hooks/useForm";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useMemo, useRef } from "react";
 import { setActiveNote } from "../../store/journal/journalSlice";
-import { startSaveNote, startUploadingFiles } from "../../store/journal/thunks";
+import {
+  startDeletingNote,
+  startSaveNote,
+  startUploadingFiles,
+} from "../../store/journal/thunks";
 import Swal from "sweetalert2";
 
 export const NoteView = () => {
@@ -41,6 +49,11 @@ export const NoteView = () => {
 
     dispatch(startUploadingFiles(target.files));
   };
+
+  const onDelete = () => {
+    dispatch(startDeletingNote());
+  };
+
   return (
     <Grid
       container
@@ -108,9 +121,15 @@ export const NoteView = () => {
           value={body}
           onChange={onInputChange}
         />
+
+        <Grid container justifyContent="end">
+          <Button onClick={onDelete} sx={{ mt: 2 }} color="error">
+            <DeleteOutline />
+          </Button>
+        </Grid>
       </Grid>
 
-      <ImageGallery images={activeNote.imageUrls}/>
+      <ImageGallery images={activeNote.imageUrls} />
     </Grid>
   );
 };
